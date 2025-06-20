1a3fd1068f560c7ddba36696c307d2cf26029d23
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2025-06-21 01:42:56 +0400

    icon change

--- app/components/NavBarTicker.tsx
+++ app/components/NavBarTicker.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useRef, useEffect } from "react"; 
-import gsap from 'gsap';
-import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 
 gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger);
@@ -11,40 +11,40 @@ export default function NewsTicker() {
   const tickerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const animateText = async () => {
-      const { SplitText } = await import('gsap/SplitText');
+      const { SplitText } = await import("gsap/SplitText");
       gsap.registerPlugin(SplitText);
 
       if (tickerRef.current) {
-        const split1 = new SplitText(tickerRef.current, { type: 'chars' });
+        const split1 = new SplitText(tickerRef.current, { type: "chars" });
         gsap.from(split1.chars, {
           scrollTrigger: {
             trigger: tickerRef.current,
-            start: 'top 80%',
-            toggleActions: 'play none none none',
+            start: "top 80%",
+            toggleActions: "play none none none",
             once: true,
           },
           opacity: 0,
           y: 20,
           stagger: 0.03,
           duration: 5,
-          ease: 'power2.out',
+          ease: "power2.out",
         });
       }
- 
     };
 
     animateText();
   }, []);
- 
 
   return (
     <div className="block fixed top-0 left-0 w-full ticker-bar text-center py-2 overflow-hidden z-50 shadow-md">
-      <div className="whitespace-nowrap h-[35px]  "  >
-        
-          <Image src='/tickerIcon.svg' alt="icon image" width={65} height={65} />
-        <span  ref={tickerRef}  className="  tracking-wide w-100 text-sm">
+      <div className="whitespace-nowrap h-[35px]  flex justify-center content-center items-center">
+        <Image src="/tickerIcon.svg" alt="icon image" width={65} height={65} />
+        <span
+          ref={tickerRef}
+          className="  tracking-wide w-100 text-sm flex item"
+        >
           Want a Free Audit?
-        </span> 
+        </span>
       </div>
     </div>
   );
